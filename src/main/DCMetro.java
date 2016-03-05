e28d2c30bbc9df44dd03f4cad259350253b4913d
mploof <jayburlage@Jays-MacBook-Air.local>
2016-03-04 21:08:23 -0500

    Fetching of line info

--- src/main/DCMetro.java
+++ src/main/DCMetro.java
@@ -1,16 +1,16 @@
 package main;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
@@ -24,51 +24,82 @@ public class DCMetro
 {		
 
 	static String JSONFileLocation = "../data/station-data.json";
+	static JSONParser parser = new JSONParser();
 	
 	private static List<MetroStation> stationList = new ArrayList<MetroStation>();
+	private static List<MetroLine> lineList = new ArrayList<MetroLine>();
 	
 	public static void initalize() 
     {
-        HttpClient httpclient = HttpClients.createDefault();
+		initLines();
+		initStations();
+    }
+	
+	private static void initLines(){
+		JSONArray lineArray = apiCall("https://api.wmata.com/Rail.svc/json/jLines", null, "Lines");
 
-        try
-        {
-            URIBuilder builder = new URIBuilder("https://api.wmata.com/Rail.svc/json/jStations");
+    	System.out.println("Lines retreived: " + lineArray.size());
+        for(int i = 0; i < lineArray.size(); i++){
+        	JSONObject lineInfo = (JSONObject) lineArray.get(i);
+    		MetroLine newLine = new MetroLine(lineInfo);
+    		lineList.add(newLine);
+        }
+	}
+	
+	private static void initStations(){
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("LineCode", "");
+        JSONArray stationArray = apiCall("https://api.wmata.com/Rail.svc/json/jStations", params, "Stations");
+        
+        // If information is available, create the list of metro station objects
+        System.out.println("Stations retreived: " + stationArray.size());
+        for(int i = 0; i < stationArray.size(); i++){
+        	JSONObject thisStation = (JSONObject) stationArray.get(i);
+    		MetroStation newStation = new MetroStation(thisStation);
+    		stationList.add(newStation);
+        }
+        createGeoJSONFile();
+    }
+	
+	private static JSONArray apiCall(String url, HashMap<String,String> params, String arrayName) {
+		System.out.println("Performaing an API call!");
+		JSONArray fetchedArray = null;
+		HttpClient httpclient = HttpClients.createDefault();
 
-            builder.setParameter("LineCode", "");
+        try {
+            URIBuilder builder = new URIBuilder(url);
+
+            if(params != null){
+	            for(int i = 0; i < params.size(); i++){
+	            	String key = (String) params.keySet().toArray()[i];
+	            	String value = (String) params.values().toArray()[i];
+	            	System.out.println("Key: " + key + " val: " + value);
+	            	builder.setParameter(key, value);
+	            }
+            }
             builder.setParameter("api_key", "bdb04a8a84d844abb6958c07735c4d2e");
 
             URI uri = builder.build();
             
             HttpGet request = new HttpGet(uri);
-            
-            JSONParser parser = new JSONParser();
            
             HttpResponse response = httpclient.execute(request);
             HttpEntity entity = response.getEntity();
             
-           
-            
-            // If information is available, create the list of metro station objects
-            if (entity != null) 
-            {
-            	JSONArray stationArray = (JSONArray) ((JSONObject) parser.parse(EntityUtils.toString(entity))).get("Stations");
-            	System.out.println("Stations retreived: " + stationArray.size());
-            	
-                for(int i = 0; i < stationArray.size(); i++){
-                	JSONObject thisStation = (JSONObject) stationArray.get(i);
-            		MetroStation newStation = new MetroStation(thisStation);
-            		stationList.add(newStation);
-                }
-            }	
-        }
-        catch (Exception e)
-        {
+	        if (entity != null) 
+	        {
+				try {
+					fetchedArray = (JSONArray) ((JSONObject) parser.parse(EntityUtils.toString(entity))).get(arrayName);
+				} catch (ParseException | org.json.simple.parser.ParseException | IOException e) {
+					e.printStackTrace();
+				}
+	        }
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        createGeoJSONFile();
-    }
-
+        return fetchedArray;
+	}
+	
 	public static List<MetroStation> getStationList(){
 		return stationList;
 	}
--- src/main/MetroLine.java
+++ src/main/MetroLine.java
@@ -2,6 +2,8 @@ package main;
 
 import java.util.List;
 
+import org.json.simple.JSONObject;
+
 import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
 
 import java.util.ArrayList;
@@ -11,8 +13,25 @@ public class MetroLine {
 	List<SimpleLinesMarker> segments = new ArrayList<SimpleLinesMarker>();
 	String code;
 	
-	MetroLine(String code){
-		this.code = code;
+    String displayName;
+    String endStationCode;
+    String internalDestination1;
+    String internalDestination2;
+    String lineCode;
+    String startStationCode;
+	
+	public MetroLine(JSONObject lineInfo){
+		setLineInfo(lineInfo);
+	}
+	
+	public void setLineInfo(JSONObject info){
+		System.out.println(info.toString());
+        this.displayName = info.get("DisplayName").toString();
+        this.endStationCode = info.get("EndStationCode").toString();
+        this.internalDestination1 = info.get("InternalDestination1").toString();
+        this.internalDestination2 = info.get("InternalDestination2").toString();
+        this.lineCode = info.get("LineCode").toString();
+        this.startStationCode = info.get("StartStationCode").toString();
 	}
 	
 	public void addStation(MetroStation station){
